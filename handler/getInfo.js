464bf7839af29256e804c41fff99dbb141c7ad64
julien <jul.thibaud85@gmail.com>
2017-09-02 14:32:37 +0200

    up

--- handler/getInfo.js
+++ handler/getInfo.js
@@ -1,13 +1,26 @@
-const cheerio                       = require('cheerio');
-const request                       = require('request-promise');
-const { isValidWetransfertUrl }     = require('../utils/utils');
+const cheerio                                               = require('cheerio');
+const request                                               = require('request-promise');
+const { isValidWetransfertUrl, formatDownloadApiUri }       = require('../utils/utils');
 
 
 const _preloaded_transfer_Regex     = /\_preloaded\_transfer\_/g;
 const removeVarDeclarationRegex     = /var[\s]*\_preloaded\_transfer\_[\s]*=/g;
 const removeLastSemicolon           = /(}\;\n)$/g;
 
-const parseFindFromHtml = function(body){ // Return a list of var
+
+const extractVar = function(text){
+    return new Promise((resolve, reject) => {
+        try{
+            const json = text.replace(removeVarDeclarationRegex, '').replace(removeLastSemicolon, '}');
+            return resolve(JSON.parse(json));
+        }
+        catch(e){
+            return reject(e.message);
+        }
+    });
+}
+
+const extractScriptContent = function(body){ // Return a list of var
     return new Promise((resolve, reject) => {
         const $ = cheerio.load(body);
         $('script').each(function(index, element) {
@@ -20,39 +33,79 @@ const parseFindFromHtml = function(body){ // Return a list of var
     });
 }
 
-const extractVar = function(text){
+const getContentInfo = function(urlObj){
     return new Promise((resolve, reject) => {
-        const json = text.replace(removeVarDeclarationRegex, '').replace(removeLastSemicolon, '}');
-        try{
-            return resolve(JSON.parse(json));
-        }
-        catch(e){
-            return reject(e.message + '  ' + json);
-        }
+        request({
+            method: 'GET',
+            uri: urlObj.href,
+            json: false,
+            simple: true,
+            resolveWithFullResponse: false
+        })
+        .then(extractScriptContent)
+        .then(extractVar)
+        .then((content) => {
+            return resolve(content);
+        })
+        .catch((err) => {
+            return reject(err);
+        })
     });
 }
 
-
-exports.getListOfFile = function(url){
+const getDownloadUri = function(urlObj){
     return new Promise((resolve, reject) => {
-        if((typeof(url) === 'string' && isValidWetransfertUrl(url)) || (typeof(url) === 'object' && url.href)){
-            request({
-                uri: url.href,
-                json: false,
-                simple: true,
-                resolveWithFullResponse: false
-            })
-            .then(parseFindFromHtml)
-            .then(extractVar)
-            .then((list) => {
-                return resolve(list);
-            })
-            .catch((err) => {
-                return reject(err);
-            })
+        const requestParams = formatDownloadApiUri(urlObj)
+        request({
+            method: 'POST',
+            uri: requestParams.uri,
+            body: requestParams.body,
+            'headers': {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+            },
+            json: true,
+            simple: true,
+            resolveWithFullResponse: false
+        })
+        .then((data) =>{
+            return resolve(data.direct_link);
+        })
+        .catch((err) => {
+            return reject(err);
+        })
+    });
+}
+
+const formatResult = function(array){
+    return new Promise((resolve, reject) => {
+        return resolve({
+            content: array[0],
+            downloadURI: array[1]
+        });
+    });
+}
+
+exports.getInfo = function(url){
+    return new Promise((resolve, reject) => {
+        if(typeof(url) === 'string'){
+            const URLObject = isValidWetransfertUrl(url);
+            if(URLObject){
+                Promise.all([getContentInfo(URLObject), getDownloadUri(URLObject)])
+                .then(formatResult)
+                .then((result) => {
+                    return resolve(result);
+                })
+                .catch((err) => {
+                    return reject(err);
+                })
+            }
+            else{
+                return reject('Unhanle url');
+            }
         }
         else{
-            return reject('Unhanle url');
+            return reject('Unhanle url - url must be a string');
         }
     });
 }
