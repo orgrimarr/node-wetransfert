818fd5bea81c4d88585df3c1dacf5badef24a7e9
orgrimarr <me@orgrimarr.fr>
2019-03-18 23:03:22 +0100

    Fix long url
    Fixes #8

--- utils/utils.js
+++ utils/utils.js
@@ -1,36 +1,43 @@
 const { parse }         = require('url');
 const request           = require("request");
+const debug             = require('debug')('wetransfert:utils')
 
 const apiVersion = "v4"
+debug("wetransfer API version: " + apiVersion)
 
 function expandUrl(shortUrl) {
     return new Promise((resolve, reject) => {
         request( { method: "HEAD", url: shortUrl, followAllRedirects: true },
-            function (error, response) {
-                if(error){
-                    return reject(error);
-                }
+        function (error, response) {
+            if(error){
+                return reject(error);
+            }
+                debug(`expandUrl: "${shortUrl}" => "${response.request.href}"`)
                 return resolve(response.request.href);
             });
     });
 }
 
-const weTransfertRegex      = /(https:\/\/wetransfer\.com\/downloads\/[0-9a-z]{10,}\/[0-9a-z]{10,}\/[0-9a-z]{4,})/;
-const weTransfertRegexShort = /(https:\/\/we\.tl\/.{5,})/;
-const weTransfertRegexMoy   = /(https:\/\/wetransfer\.com\/downloads\/[0-9a-z]{10,}\/[0-9a-z]{4,})/;
+const weTransfertRegex         = /(https:\/\/wetransfer\.com\/downloads\/[0-9a-zA-Z]{10,}\/[0-9a-zA-Z]{10,}\/[0-9a-zA-Z]{4,})/i;
+const weTransfertRegexShort    = /(https:\/\/we\.tl\/.{5,})/i;
+const weTransfertRegexMedium   = /(https:\/\/wetransfer\.com\/downloads\/[0-9a-zA-Z]{10,}\/[0-9a-zA-Z]{4,})/i;
 
 exports.isValidWetransfertUrl = function(url){
-    if(weTransfertRegex.exec(url) !== null || weTransfertRegexShort.exec(url) !== null || weTransfertRegexMoy.exec(url) !== null){
+    if(weTransfertRegex.exec(url) !== null || weTransfertRegexShort.exec(url) !== null || weTransfertRegexMedium.exec(url) !== null){
+        debug(`isValidWetransfertUrl: true`)
         return parse(url);
     }
     else{
+        debug(`isValidWetransfertUrl: false`)
         return false
     }
 }
 
 exports.formatDownloadApiUri = async function(urlObj){
     try{
+        // Short link 
         if(weTransfertRegexShort.exec(urlObj.href) !== null){
+            debug("formatDownloadApiUri: short_url")
             const resp = await expandUrl(urlObj.href)
             const newURL = parse(resp);
             if(newURL){
@@ -43,21 +50,27 @@ exports.formatDownloadApiUri = async function(urlObj){
                 }
             }
         }
-        if(weTransfertRegexMoy.exec(urlObj.href) !== null){
-            const [, , urlID, hash] = urlObj.pathname.split('/');
-            return  {
-                uri: `https://wetransfer.com/api/${apiVersion}/transfers/${urlID}/download`,
-                body: {
-                    "security_hash": hash
-                }
-            }
-        }
-        else{
+
+        // Nomal url
+        if(weTransfertRegex.exec(urlObj.href) !== null){
+            debug("formatDownloadApiUri: normal_url")
             const [, , urlID, recipient_id, hash] = urlObj.pathname.split('/');
             return  {
                 uri: `https://wetransfer.com/api/${apiVersion}/transfers/${urlID}/download`,
                 body: {
                     "recipient_id": recipient_id,
+                    "security_hash": hash
+                }
+            }
+        }
+
+        // Medium  url
+        if(weTransfertRegexMedium.exec(urlObj.href) !== null){
+            debug("formatDownloadApiUri: medium_url")
+            const [, , urlID, hash] = urlObj.pathname.split('/');
+            return  {
+                uri: `https://wetransfer.com/api/${apiVersion}/transfers/${urlID}/download`,
+                body: {
                     "security_hash": hash
                 }
             }
